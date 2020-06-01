9e4c3b2ac136ef8b4e8212f0f0301ac83194a13e
asharma414 <abhi.sharm.414@gmail.com>
2020-05-31 21:11:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.querySelector("#main").remove()
+
+let element = document.createElement('h1')
+element.id = "victory"
+element.innerHTML = "Abhinav is the champion"
+const newHeader = document.body.appendChild(element)
